85f4f23df24e6a83e339274315be8a2edf2502cb
Yogev Barber <yogevbarber@gmail.com>
2020-04-10 21:33:29 +0300

    Initial Commit

---
+++ RemoteDebugger/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RemoteDebugger
+//
+//  Created by Yogev Barber on 10/04/2020.
+//  Copyright © 2020 Yogev Barber. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RemoteDebugger/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RemoteDebugger
+//
+//  Created by Yogev Barber on 10/04/2020.
+//  Copyright © 2020 Yogev Barber. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
