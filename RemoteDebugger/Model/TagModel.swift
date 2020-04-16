b69bc6e97113a91c750f62f15055485265f9aec1
Yogev Barber <yogevbarber@gmail.com>
2020-04-16 15:10:07 +0300

    ...

---
+++ RemoteDebugger/Model/TagModel.swift
@@ -0,0 +1,16 @@
+//
+//  Tag.swift
+//  RemoteDebugger
+//
+//  Created by Yogev Barber on 11/04/2020.
+//  Copyright © 2020 Yogev Barber. All rights reserved.
+//
+
+import SwiftUI
+
+struct TagModel: Identifiable, Decodable {
+    
+    /// unique id
+    var id: String = UUID().uuidString
+    let tag: Tag
+}
